61d5af31334a58d4dff686ae6a695509828bbf33
Chrisma <chrismanguimbi@gmail.com>
2022-04-24 22:30:49 +0200

    commit du 24 avril

--- cypress/integration/demoblaze.spec.js
+++ cypress/integration/demoblaze.spec.js
@@ -1,4 +1,11 @@
 /// <reference types="cypress" />
+const { faker } = require("@faker-js/faker");
+
+//const username = faker.internet.userName();
+const username = faker.name.firstName();
+const password = faker.internet.password(10);
+console.log(username);
+console.log(password);
 
 describe("demoblaze", () => {
   it("Afficher https://www.demoblaze.com", () => {
@@ -7,8 +14,29 @@ describe("demoblaze", () => {
 
   it("Sign up", () => {
     cy.get("#signin2").click();
-    cy.get("#signInModalLabel").should("contains.text", "Sign up");
-    cy.get("#sign-username").type("user");
-    cy.get("#sign-password").type("mdp");
+    cy.get("#signInModalLabel")
+      .should("be.visible")
+      .and("contains.text", "Sign up");
+    cy.get("#sign-username").type(username);
+    cy.get("#sign-password").type(password);
+    cy.get(
+      "#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
+    ).click();
+    cy.get("#login2").should("be.visible").and("contains.text", "Log in");
+    console.log(username);
+    console.log(password);
+  });
+
+  it("Log in", () => {
+    cy.get("#login2").click();
+    cy.get("#logInModalLabel")
+      .should("be.visible")
+      .and("contains.text", "Log in");
+    cy.get("#loginusername").type(username);
+    cy.get("#loginpassword").type(password);
+    cy.get(
+      "#logInModal > div > div > div.modal-footer > button.btn.btn-primary"
+    ).click();
+    cy.get("#nameofuser").should("contains.text", "Welcome " + username);
   });
 });
