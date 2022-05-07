b53c8e7711577f9aa393a15356dee50c356d18f0
Chrisma <chrismanguimbi@gmail.com>
2022-05-07 09:36:12 +0200

    Commit fin du kata 02

--- cypress/integration/demoblaze.spec.js
+++ cypress/integration/demoblaze.spec.js
@@ -1,11 +1,15 @@
 /// <reference types="cypress" />
 const { faker } = require("@faker-js/faker");
 
-//const username = faker.internet.userName();
-const username = faker.name.firstName();
+const username = faker.internet.userName();
+const email = faker.internet.email();
+const name = faker.name.firstName();
 const password = faker.internet.password(10);
-console.log(username);
-console.log(password);
+const country = faker.address.country();
+const city = faker.address.city();
+const card = faker.finance.creditCardNumber();
+const month = faker.date.month();
+const year = "2026";
 
 describe("demoblaze", () => {
   it("Afficher https://www.demoblaze.com", () => {
@@ -17,26 +21,74 @@ describe("demoblaze", () => {
     cy.get("#signInModalLabel")
       .should("be.visible")
       .and("contains.text", "Sign up");
-    cy.get("#sign-username").type(username);
-    cy.get("#sign-password").type(password);
+    cy.get("#sign-username")
+      .should("be.visible")
+      .type(username)
+      .should("have.value", username);
+    cy.get("#sign-password").should("be.visible").type(password);
     cy.get(
       "#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
     ).click();
     cy.get("#login2").should("be.visible").and("contains.text", "Log in");
-    console.log(username);
-    console.log(password);
   });
 
   it("Log in", () => {
-    cy.get("#login2").click();
+    cy.get("#login2").click({ force: true });
+    cy.wait(1000);
     cy.get("#logInModalLabel")
       .should("be.visible")
       .and("contains.text", "Log in");
-    cy.get("#loginusername").type(username);
-    cy.get("#loginpassword").type(password);
+    cy.get("#loginusername")
+      .should("be.visible")
+      .type(username)
+      .should("have.value", username);
+    cy.get("#loginpassword").should("be.visible").type(password);
     cy.get(
-      "#logInModal > div > div > div.modal-footer > button.btn.btn-primary"
-    ).click();
+      "#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
+    )
+      .should("be.visible")
+      .click({ force: true });
+    cy.url().should("eq", "https://www.demoblaze.com/");
+  });
+
+  it("Ajouter un produit au panier", () => {
     cy.get("#nameofuser").should("contains.text", "Welcome " + username);
+    cy.get(".card-block").eq(2).find(".card-title").click();
+    cy.get(".name").should("be.visible").and("contains.text", "Nexus 6");
+    cy.get(".btn-success").click();
+    cy.get("#cartur").click();
+    cy.contains("Nexus 6").should("be.visible").and("contains.text", "Nexus 6");
+  });
+
+  it("Confirmer la commande et valider le paiement", () => {
+    cy.get("#page-wrapper > div > div.col-lg-1 > button").click();
+    cy.get("#name").type(name);
+    cy.get("#country").type(country);
+    cy.get("#city").type(city);
+    cy.get("#card").type(card);
+    cy.get("#month").type(month);
+    cy.get("#year").type(year);
+    cy.get(
+      "#orderModal > div > div > div.modal-footer > button.btn.btn-primary"
+    ).click();
+    cy.get("body > div.sweet-alert.showSweetAlert.visible > h2")
+      .should("be.visible")
+      .and("contains.text", "Thank you for your purchase!");
+    cy.get(
+      "body > div.sweet-alert.showSweetAlert.visible > div.sa-button-container > div > button"
+    ).click();
+    cy.get("#cartur").click();
+    cy.get("#tbodyid").find("tr").should("not.exist");
+  });
+
+  it("Supprimer un produit du panier", () => {
+    cy.get(".nav-link").eq(0).and("contains.text", "Home").click();
+    cy.get(".card-block").eq(2).find(".card-title").click();
+    cy.get(".name").should("be.visible").and("contains.text", "Nexus 6");
+    cy.get(".btn-success").click();
+    cy.get("#cartur").click();
+    cy.contains("Nexus 6").should("be.visible").and("contains.text", "Nexus 6");
+    cy.get(".success > :nth-child(4) > a").click();
+    cy.get("#tbodyid").find("tr").should("not.exist");
   });
 });
